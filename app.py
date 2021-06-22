51c22098586774418a5a31ec13f70797098d3a87
park-sohyeon <64852208+park-sohyeon@users.noreply.github.com>
2021-06-22 12:42:55 +0900

    안드로이드 버전

--- app.py
+++ app.py
@@ -3,8 +3,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!!!'
+    print(__name__)
+    return 'Hello World'
+
+@app.route('/one')
+def hello_one():
+    return 'Hello one'
+
+@app.route('/two')
+def hello_two():
+    return 'Hello two'
 
 if __name__== '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host = '0.0.0.0')
 
