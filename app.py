d71f3885c2f8290baed40767c12c84a6c121e9b8
park-sohyeon <64852208+park-sohyeon@users.noreply.github.com>
2021-06-27 18:33:56 +0900

    model 불러오기 완료

--- app.py
+++ app.py
@@ -1,62 +1,49 @@
-# import flask
-# from flask import Flask, request, render_template, jsonify
-# import sklearn.externals
-# import joblib
-# import numpy as np
-# from scipy import misc
-
-# # Flask 애플리케이션과 Keras 모델을 초기화합니다.
-# app = Flask(__name__)
-
-
-# @app.route('/') 
-# @app.route('/index')
-# def index():
-#     return flask.render_template("hello.html")
+import flask
+from flask import Flask, request, render_template, jsonify
+import numpy as np
+from tensorflow.keras.models import load_model, save_model
+import matplotlib.pyplot as plt
+import numpy
+import pandas as pd
 
 
 
-# @app.route('/predict', methods = ['POST'])  
-# def predict():
-#     data = {"success": False}
+# json_file = open("model.json", "r") 
+# loaded_model = json_file.read() 
+# json_file.close() 
 
-#     return flask.jsonify(data)
-#     #if request.method == 'POST':
-#         #return render_template('hello.html')
-
-    
-
-# @app.route('/environments/<language>')  
-# def environments(language):  
-#      return jsonify({"language":language}) #받아온 데이터 다시 전송 
-
-# if __name__== '__main__': # 모델로드
-   
-#     model = joblib.load('model.h5')
-#     app.run(debug = True)
-
-from flask import Flask, render_template, jsonify
-from flask.globals import request
+# model = model_from_json(loaded_model)
 
 
+
+# Flask 애플리케이션과 Keras 모델을 초기화합니다.
 app = Flask(__name__)
 
 
-@app.route('/')  
-def hello_world():
-    print(__name__)
-    return render_template("hello.html")
+@app.route('/') 
+@app.route('/index')
+def index():
+    return "모델 불러오기 완료"
+    # return flask.render_template(model)
 
-@app.route('/userLogin', methods = ['POST'])  
-def userLogin():
-    user = request.get_json() #json 데이터를 받아옴
-    return jsonify(user) #받아온 데이터 다시 전송
 
-@app.route('/environments/<language>') 
+@app.route('/predict', methods = ['POST'])  
+def predict():
+#     data = {"success": False}
+
+#     return flask.jsonify(data)
+    if request.method == 'POST':
+        return render_template('hello.html')
+
+    
+
+@app.route('/environments/<language>')  
 def environments(language):  
-    return jsonify({"language":language}) #받아온 데이터 다시 전송 
+     return jsonify({"language":language}) #받아온 데이터 다시 전송 
 
-if __name__== '__main__':
+if __name__== '__main__': # 모델로드
+    
+    model = load_model('model.h5')
     app.run(debug = True)
 
 
