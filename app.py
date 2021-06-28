052ee822a84b063ec1e41c747900f323ef00adaf
park-sohyeon <64852208+park-sohyeon@users.noreply.github.com>
2021-06-28 23:05:10 +0900

    db 연결 오류

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import flask
-from flask import Flask, request, render_template, jsonify
+import pymysql
+from flask import Flask, request, render_template, jsonify, json, redirect, url_for, send_from_directory
 import numpy as np
 from tensorflow.keras.models import load_model, save_model
 import matplotlib.pyplot as plt
 import numpy
 import pandas as pd
+from flask_restful import Resource, Api
 
 
 
@@ -18,14 +20,49 @@ import pandas as pd
 
 # Flask 애플리케이션과 Keras 모델을 초기화합니다.
 app = Flask(__name__)
+# api = Api(app)
+
+# class RegistUser(Resource):
+#     def post(self):
+#         return {'result': 'ok'}
+ 
+# api.add_resource(RegistUser, '/user')
 
 
 @app.route('/') 
-@app.route('/index')
 def index():
     return "모델 불러오기 완료"
-    # return flask.render_template(model)
 
+# @app.route('/predict', methods=['GET', 'POST'])
+# def upload_file():
+#     if request.method == 'POST':
+#         print(flask.request.files)
+
+conn = pymysql.connect(host='database-1.cpjkudy7ec1y.ap-northeast-2.rds.amazonaws.com',
+                        user='admin',
+                        password='0cN1KvzzVeQuO7xc7fmt',
+                        db='database-1',
+                        charset='utf8')
+curs = conn.cursor()
+
+if conn.open:
+    print('connected')
+
+@app.route('/test', methods = ['POST'])
+def postJsonHandler():
+    print (request.is_json)
+    content = request.get_json()
+    print (content)
+    id = content['id']
+    print ("#####id:",id)
+    sql = "select * from users"
+    curs.execute(sql)
+    rows = curs.fetchall()
+    print(rows)
+    data = {'id':'sohyeon'}
+    return json.dumps(data)
+
+app.run(host='0.0.0.0', port= 3306)
 
 @app.route('/predict', methods = ['POST'])  
 def predict():
@@ -44,6 +81,7 @@ def environments(language):
 if __name__== '__main__': # 모델로드
     
     model = load_model('model.h5')
+    #model.summary()
     app.run(debug = True)
 
 
